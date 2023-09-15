e768c1a8ef55504544240c601955ee0197ae4f8e
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-09-15 09:11:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 from Interface import *
 
-window = ctk.CTk()
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
-window.title("Home page")
-window.geometry("600x400")
-label = ctk.CTkLabel(master=window, text="Sing in ")  # tittle
-label.pack(pady=12, padx=10)
-
-button = ctk.CTkButton(master=window, text="login", command=login)
-button.pack(pady=12, padx=10)
-# Creamos el espacio entre el primer boton y el segundo boton
-
-
-window.mainloop()
+start()
