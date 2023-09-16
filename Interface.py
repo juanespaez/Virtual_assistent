12bfd8c9a0a4822e6d0e5b98ba6456295a9403f3
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-09-16 00:55:22 -0500

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,109 +1,153 @@
-import customtkinter as ctk
+from tkinter import *
+import os
 
+#------------------------ Crearemos una funcion que se encargara de registrar el usuario ---------------------
 
-class slide_panel(ctk.CTkFrame):
-    def __init__(self, parent, start_pos, end_pos):
-        super().__init__(master=parent)
+def registrar_usuario():
+    usuario_info = usuario.get() #Obetnemos la informacion alamcenada en usuario
+    contra_info = contra.get() #Obtenemos la informacion almacenada en contra
 
-        # general atributes
-        self.start_pos = start_pos
-        self.end_pos = end_pos - 0.03
-        self.width = abs(start_pos - end_pos)
+    archivo = open(usuario_info, "w") #Abriremos la informacion en modo escritura
+    archivo.write(usuario_info + "\n")   #escribimos la info
+    archivo.write(contra_info)
+    archivo.close()
 
-        # animation logic
-        self.pos = start_pos
-        self.in_start_pos = True
+    #Limpiaremos los text variable
+    usuario_entrada.delete(0, END)
+    contra_entrada.delete(0, END)
 
-        # layout
-        self.place(relx=start_pos, rely=0.05, relwidth=self.width, relheight=0.9)
+    #Ahora le diremos al usuario que su registro ha sido exitoso
+    Label(pantalla1, text = "Registro Convencional Exitoso", fg = "green", font = ("Calibri",11)).pack()
+    
+def registro():
+    global usuario
+    global contra  #Globalizamos las variables para usarlas en otras funciones
+    global usuario_entrada
+    global contra_entrada
+    global pantalla1
+    pantalla1 = Toplevel(pantalla) #Esta pantalla es de un nivel superior a la principal
+    pantalla1.title("Registro")
+    pantalla1.geometry("300x250")  #Asignamos el tamaño de la ventana
+    
+    #--------- Empezaremos a crear las entradas ----------------------------------------
+    
+    usuario = StringVar()
+    contra = StringVar()
+    
+   
+    #Label(pantalla1, text = "").pack()  #Dejamos un poco de espacio
+    Label(pantalla1, text = "debe asignar usuario y contraseña:").pack()
+    Label(pantalla1, text = "").pack()  #Dejamos un poco de espacio
+    Label(pantalla1, text = "Usuario * ").pack()  #Mostramos en la pantalla 1 el usuario
+    usuario_entrada = Entry(pantalla1, textvariable = usuario) #Creamos un text variable para que el usuario ingrese la info
+    usuario_entrada.pack()
+    Label(pantalla1, text = "Contraseña * ").pack()  #Mostramos en la pantalla 1 la contraseña
+    contra_entrada = Entry(pantalla1, textvariable = contra) #Creamos un text variable para que el usuario ingrese la contra
+    contra_entrada.pack()
+    Label(pantalla1, text = "").pack()  #Dejamos un espacio para la creacion del boton
+    Button(pantalla1, text = "Registrarse", width = 15, height = 1, command = registrar_usuario).pack()  #Creamos el boton
 
-    def animate(self):
-        if self.in_start_pos:
-            self.animate_forward()
-        else:
-            self.animate_backward()
-
-    def animate_forward(self):
-        if self.pos > self.end_pos:
-            self.pos -= 0.008
-            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
-            self.after(10, self.animate_forward)
-        else:
-            self.in_start_pos = False
-
-    def animate_backward(self):
-        if self.pos < self.start_pos:
-            self.pos += 0.008
-            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
-            self.after(10, self.animate_backward)
-        else:
-            self.in_start_pos = True
-
+##############################################################################################################################################
 
 def chat():
-    window = ctk.CTk()
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("dark-blue")
-    window.title("Cortana")
-    window.geometry("600x400")
+    global my_text
+    global root
+    root = Tk()
+    root.title("cortana")
+    root.geometry("800x600")
 
-    animated_panel = slide_panel(window, 1.0, 0.7)
-    ctk.CTkLabel(animated_panel, text="Hello, my name is Cortana").pack(expand=True, fill="both", padx=2, pady=10)
-    ctk.CTkTextbox(animated_panel).pack(expand=True, fill="both")
-    ctk.CTkButton(animated_panel, text="Send", corner_radius=0).pack(expand=True, fill="both", padx=2, pady=10)
+    my_text = Text(root, width=60, height=20)
+    my_text.pack(pady=20)
 
-    # botton
-    button_x = 0.5
-    button = ctk.CTkButton(window, text="Start", command=animated_panel.animate)
-    button.place(relx=button_x, rely=0.5, anchor="center")
 
-    # start
-    window.mainloop()
+    button_frame = Frame(root)
+    button_frame.pack()
 
+    clear_button = Button(button_frame, text="clear", command=clear)
+    clear_button.grid(row=0, column=0)
+
+    get_text_button = Button(button_frame, text="get_text", command=get_text)
+    get_text_button.grid(row=0, column=1)
+
+    root.mainloop()
+
+def clear():
+    my_text.delete(1.0,END) # delete the texts 
+    
+def get_text():
+    value = my_text.get(1.0, END)
+    print(value)
+    my_label = Label(root, text="value").pack(pady=20)
+    #------------  --------------------
+    
+
+def verificacion_login():
+    log_usuario = verificacion_usuario.get()
+    log_contra = verificacion_contra.get()
+
+    usuario_entrada2.delete(0, END)
+    contra_entrada2.delete(0, END)
+
+    lista_archivos = os.listdir()   #Vamos a importar la lista de archivos con la libreria os
+    if log_usuario in lista_archivos:   #Comparamos los archivos con el que nos interesa
+        archivo2 = open(log_usuario, "r")  #Abrimos el archivo en modo lectura
+        verificacion = archivo2.read().splitlines()  #leera las lineas dentro del archivo ignorando el resto
+        if log_contra in verificacion:
+            print("Inicio de sesion exitoso")
+            Label(pantalla2, text = "Inicio de Sesion Exitoso", fg = "green", font = ("Calibri",11)).pack()
+            chat()
+        else:
+            print("Contraseña incorrecta, ingrese de nuevo")
+            Label(pantalla2, text = "Contraseña Incorrecta", fg = "red", font = ("Calibri",11)).pack()
+    else:
+        print("Usuario no encontrado")
+        Label(pantalla2, text = "Usuario no encontrado", fg = "red", font = ("Calibri",11)).pack()
 
 def login():
-    ctk.set_appearance_mode("dark")  # system, dark or light/ the color - background
-    ctk.set_default_color_theme("dark-blue")  # darkblue, green, blue - login botton
-    root = ctk.CTk()
-    root.title("Login")
-    root.geometry("500x300")
-    frame = ctk.CTkFrame(root)
-    frame.pack(pady=20, padx=60, fill="both", expand=True)
+    global pantalla2
+    global verificacion_usuario
+    global verificacion_contra
+    global usuario_entrada2
+    global contra_entrada2
+    
+    pantalla2 = Toplevel(pantalla)
+    pantalla2.title("Login")
+    pantalla2.geometry("300x250")   #Creamos la ventana
+    
+    Label(pantalla2, text = "debe asignar usuario y contraseña:").pack()
+    Label(pantalla2, text = "").pack()  #Dejamos un poco de espacio
+    
+    verificacion_usuario = StringVar()
+    verificacion_contra = StringVar()
+    
+    #---------------------------------- Ingresamos los datos --------------------------
+    Label(pantalla2, text = "Usuario * ").pack()
+    usuario_entrada2 = Entry(pantalla2, textvariable = verificacion_usuario)
+    usuario_entrada2.pack()
+    Label(pantalla2, text = "Contraseña * ").pack()
+    contra_entrada2 = Entry(pantalla2, textvariable = verificacion_contra)
+    contra_entrada2.pack()
+    Label(pantalla2, text = "").pack()
+    Button(pantalla2, text = "Iniciar sesion", width = 20, height = 1, command = verificacion_login).pack()
 
-    label = ctk.CTkLabel(master=frame,
-                         text="Welcome to our virtual asistent please enter the following steps: ")  # tittle
-    label.pack(pady=12, padx=10)
-    label = ctk.CTkLabel(master=frame, text="First add your username then your password: ")  # tittle
-    label.pack(pady=12, padx=10)
+    #------------ Vamos a crear el boton para hacer el login facial --------------------
+    Label(pantalla2, text = "").pack()
 
-    # pantalla2.geometry("500x300")   #create a window
+        
+#------------------------- Funcion de nuestra pantalla principal ------------------------------------------------
+    
+def pantalla_principal():
+    global pantalla          #Globalizamos la variable para usarla en otras funciones
+    pantalla = Tk()
+    pantalla.geometry("800x600")  #Asignamos el tamaño de la ventana 
+    pantalla.title("Cortana")       #Asignamos el titulo de la pantalla
+    Label(text = "Welcome", width = "300", height = "2", font = ("Verdana", 13)).pack() #Asignamos caracteristicas de la ventana
+    
+#------------------------- Vamos a Crear los Botones ------------------------------------------------------
+    
+    Label(text = "").pack()  #Creamos el espacio entre el titulo y el primer boton
+    Button(text = "Iniciar Sesion", height = "2", width = "30", command = login).pack()
+    Label(text = "").pack() #Creamos el espacio entre el primer boton y el segundo boton
+    Button(text = "Registro", height = "2", width = "30", command = registro).pack()
 
-    # Label(pantalla2, text = "").pack()  #some space in between
-
-    # insert data
-    user2 = ctk.CTkEntry(master=frame, placeholder_text="username")  # text box under the title
-    user2.pack(pady=12, padx=10)
-
-    entry2 = ctk.CTkEntry(master=frame, placeholder_text="password")  # text box under the title
-    entry2.pack(pady=12, padx=10)
-
-    button = ctk.CTkButton(master=frame, text="login", command=chat)  # botton using the login funtion
-    button.pack(pady=12, padx=10)
-
-    return root.mainloop()
-
-
-def start():
-    window = ctk.CTk()
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("dark-blue")
-    window.title("Home page")
-    window.geometry("600x400")
-    label = ctk.CTkLabel(master=window, text="Sing in ")  # tittle
-    label.pack(pady=12, padx=10)
-
-    button = ctk.CTkButton(master=window, text="login", command=login)
-    button.pack(pady=12, padx=10)
-    # Creamos el espacio entre el primer boton y el segundo boton
-
-    window.mainloop()
+    pantalla.mainloop()
