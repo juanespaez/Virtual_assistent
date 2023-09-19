772af9a59f4bb7743b7142ce20f97b40624bb984
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-09-19 00:32:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 from Interface import *
+from user import *
+from database import *
 
 pantalla_principal()
+
+type_search = int(input("""What is your desire?
+    1 - general question knowledge theme
+    2 - specific question search
+    3 - play a song
+    > """))
+search = User(type_search)
+
+Database(type_search, search)
