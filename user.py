68fb7578a813d564963808235cf45f59a163b91e
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-09-19 00:42:25 -0500

    Update user.py

--- user.py
+++ user.py
@@ -3,18 +3,18 @@
 class User:
     def __init__(self, type_s):
         self.type = type_s
-        self.question = ""
+        self.question = "" # empyty string as default string
 
         if self.type == 1:
-            self.question = input("ask: ").__str__()
+            self.question = input("ask: ").__str__() # we use the __str__ so the program later ca read it as a str type
 
         elif self.type == 2:
-            self.question = input("What do you want to know? ")
+            self.question = input("What do you want to know? ") # during the code it was unnecessary to use __str__ here
 
         elif self.type == 3:
             self.question = input("What song you want to play? ").__str__()
 
-    def __str__(self):
+    def __str__(self): # funtion that lets the question be return with no issues 
         return self.question
 
 
