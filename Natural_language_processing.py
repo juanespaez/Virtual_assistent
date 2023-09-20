8afed16905aaffaa601b73ecf43fe61cdd4e434f
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-09-20 00:10:09 -0500

    Update Natural_language_processing.py

--- Natural_language_processing.py
+++ Natural_language_processing.py
@@ -1,1 +1,15 @@
-# aqui iran los modelos de compresion de vocabulario
+import nltk
+from nltk.stem.porter import PorterStemmer
+import json
+
+nltk.download("punkt")
+stemmer = PorterStemmer()
+
+def tokenize(setence):
+    return nltk.word_tokenize(setence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenize_sentence, all_words):
+    pass# aqui iran los modelos de compresion de vocabulario
