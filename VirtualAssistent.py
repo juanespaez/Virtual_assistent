e5607ce24c0b42535eb5b48e54601454189f4814
Juan José <JuanJRiosP9@gmail.com>
2023-09-21 15:30:39 -0500

    Update VirtualAssistent.py
    Correcciones de ortografia

--- VirtualAssistent.py
+++ VirtualAssistent.py
@@ -3,16 +3,15 @@ from Response import *
 
 class VirtualAssistent:
 
-    def __init__(self, name, task):
+    def __init__(self, name='Cortana'):
         self.name = name
-        self.task = task
 
     def welcome(self):
         print(f"""Hello, my name is {self.name}, i'm here for help you.
         What do you want to do?
         1. General knowledge question
         2. Specific search
-        3. Play a son
+        3. Play a song
         4. Question with context """)
 
         option = int(input('Choose one option: '))
