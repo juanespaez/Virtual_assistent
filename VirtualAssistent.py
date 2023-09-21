94f830043ecba8f585423a5eea9b166c859a4d50
Juan José <JuanJRiosP9@gmail.com>
2023-09-21 14:05:04 -0500

    Update VirtualAssistent.py
    Fuction Welcome

--- VirtualAssistent.py
+++ VirtualAssistent.py
@@ -1,6 +1,19 @@
+from Response import *
+
+
 class VirtualAssistent:
 
     def __init__(self, name, task):
         self.name = name
         self.task = task
-        
+
+    def welcome(self):
+        print(f"""Hello, my name is {self.name}, i'm here for help you.
+        What do you want to do?
+        1. 
+        2.
+        3.
+        4. Question with context """)
+
+        option = int(input('Choose one option: '))
+        return option
