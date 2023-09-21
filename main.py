7d9f724f035959a067062b7374d2322dfcc5fd83
Juan José <JuanJRiosP9@gmail.com>
2023-09-21 15:24:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 from Interface import *
-from user import *
-from database import *
+from VirtualAssistent import *
+from User import *
+from NaturalLanguajeModel import *
+from Response import *
+
+while True:
+    Cortana = VirtualAssistent()
+    option = Cortana.welcome()
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+
+    elif option == 4:
+        doubt = User(option)
+        bert = BertResponse(doubt.question)
+        answer = bert.bertanswer()
+        print(answer)
+        print('-'*20)
+        change = input(print('Do you wanna use the same context?: '))
+        while change.lower() == 'yes':
+            new_doubt = input("What is your new question?: ")
+            doubt.question['question'] = new_doubt
+            bert = BertResponse(doubt.question)
+            answer = bert.bertanswer()
+            print(answer)
+            print('-'*20)
+            change = input(print('Do you wanna use the same context?: '))
+
+    elif option == 5:
+        print('Bye Bye')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
 
 pantalla_principal()
 
