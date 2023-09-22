5cc05b71678b19d974912117441df51b8c824a5e
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-09-21 23:10:39 -0500

    Update user.py

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 
 
 class User:
-    def __init__(self, type_s):
+    def __init__(self, type_s): # tipo s sera el tipo de busqueda para asi definir que funciin aplicar
         self.type = type_s
         self.question = "" # empyty string as default string
 
@@ -12,7 +12,7 @@ class User:
             self.question = input("What do you want to know? ") # during the code it was unnecessary to use __str__ here
 
         elif self.type == 3:
-            self.question = input("What song you want to play? ").__str__()
+            self.question = input("What song you want to play? ").__str__() # usamos la funcion str para que se logre usar el objeto como un str
 
         elif self.type == 4:
             qainput = dict()
