8fab2247288e6256e9f7432fc9f2ccdcad0e4a2f
Esteban <122425146+juanespaez@users.noreply.github.com>
2023-10-29 19:56:06 -0500

    Update Natural_language_processing.py

--- Natural_language_processing.py
+++ Natural_language_processing.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
-import json
+import numpy as np
 
 nltk.download("punkt")
 stemmer = PorterStemmer()
@@ -8,10 +8,10 @@ stemmer = PorterStemmer()
 
 class NLP:
 
-    def __init__(self, sentece, word):
+    def __init__(self, sentece=" ", word=" "):
         self.sentence = sentece
         self.word = word
-        
+
     def tokenize(self):
         return nltk.word_tokenize(self.sentence)
 
@@ -19,5 +19,10 @@ class NLP:
         return stemmer.stem(self.word.lower())
 
     def bag_of_words(self, tokenize_sentence, all_words):
-        pass  # aqui iran los modelos de compresion de vocabulario
+        tokenize_sentence = [NLP.stem(w) for w in tokenize_sentence]
+        bag = np.zeros(len(all_words), dtype=np.float32)
+        for idx, w in enumerate(all_words):
+            if w in tokenize_sentence:
+                bag[idx] = 1.0
+        return bag
 
